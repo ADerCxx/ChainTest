789b272c97e6906ddba12bdfb8ec322aafa2b8c2
cxx <1226298223@qq.com>
2025-02-21 19:23:18 +0800

    stylelint配置基本完成，stylelint生效检验

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
     <TheWelcome />
+    <div class="chaintest"></div>
   </main>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import TheWelcome from '../components/TheWelcome.vue'
+
+const fontColor = ref('#999')
+</script>
+
+<style scoped lang="less">
+.chaintest {
+  display: flex;
+  flex: 1;
+  align-items: center;
+  width: 13px;
+  height: 20px;
+  color: v-bind(fontColor);
+  background-color: blue;
+}
+</style>
